f605fccc355c9d76eb777149e3ac1f8137fa614b
Nidhi Prakash <np562019@gmail.com>
2024-10-25 23:42:09 +0530

    checking

--- index.js
+++ index.js
@@ -1,46 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import { userRouter } from "./routes/userRouter.js";
-// import { createDbConnection } from "./config/DbConnection.js";
-// import { contactsRouter } from "./routes/contactsRouter.js";
-// import path from "path";
-// import { fileURLToPath } from "url";
-
-// const app = express();
-// const PORT = 5000;
-
-// app.use(
-//   cors({
-//     origin: [
-//       "https://phone-book-frontend-gamma.vercel.app/",
-//       "http://localhost:3000",
-//     ],
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     credentials: true,
-//   })
-// );
-
-// app.use(express.json());
-
-// app.use("/user", userRouter);
-// app.use("/contacts", contactsRouter);
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// app.use(express.static(path.join(__dirname, "../phone-book/build")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../phone-book/build/index.html"));
-// });
-
-// const runServer = async () => {
-//   await createDbConnection();
-//   app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-//   });
-// };
-
-// runServer();
-
 import express from "express";
 import cors from "cors";
 import { userRouter } from "./routes/userRouter.js";
@@ -64,6 +21,16 @@ app.use(
     credentials: true,
   })
 );
+
+// Remove this block, as `cors()` already manages these headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // Middleware to parse JSON bodies
 app.use(express.json());
